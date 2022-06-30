d6e62a08d131e01709a9c6cf5bec253d3d764fa9
Goban <gobanest@gmail.com>
2022-06-30 23:30:14 +0900

    [project] Setting for code base UI #5

--- TossCloneApp/ViewController.swift
+++ TossCloneApp/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
 
@@ -13,5 +14,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+}
 
+struct ViewControllerRepresentable: UIViewControllerRepresentable {
+    typealias UIViewControllerType = ViewController
+
+    func makeUIViewController(context: Context) -> ViewController {
+        return ViewController()
+    }
+
+    func updateUIViewController(_ uiViewController: ViewController, context: Context) {
+    }
+}
+
+@available(iOS 13.0.0, *)
+struct ViewPreview: PreviewProvider {
+    static var previews: some View {
+        ViewControllerRepresentable()
+    }
 }
