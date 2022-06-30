a79130fb0fcff34bb70cd0e5a5060beba1d0d5a2
Goban <gobanest@gmail.com>
2022-06-30 21:07:17 +0900

    [feat] #1 create project

---
+++ TossCloneApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TossCloneApp
+//
+//  Created by Lee Myeonghwan on 2022/06/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
