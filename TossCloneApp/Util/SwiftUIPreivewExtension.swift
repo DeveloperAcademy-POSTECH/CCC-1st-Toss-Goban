b20a18f720f588eccffeac907ca27e4acb175210
Goban <gobanest@gmail.com>
2022-07-01 21:15:30 +0900

    [refactor] import swiftUIPreview

---
+++ TossCloneApp/Util/SwiftUIPreivewExtension.swift
@@ -0,0 +1,24 @@
+//
+//  SwiftUIPreivewExtension.swift
+//  TossCloneApp
+//
+//  Created by Lee Myeonghwan on 2022/07/01.
+//
+
+import SwiftUI
+extension UIViewController {
+    private struct Preview: UIViewControllerRepresentable {
+            let viewController: UIViewController
+
+            func makeUIViewController(context: Context) -> UIViewController {
+                return viewController
+            }
+
+            func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+            }
+        }
+
+        func toPreview() -> some View {
+            Preview(viewController: self)
+        }
+}
